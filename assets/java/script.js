41d0510758a3b9b6d7ce3ea86653f73755ffd967
StevenTraboulay <khaleeltraboulay@gmail.com>
2020-09-22 18:02:49 -0400

    modified form dropdowns

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,17 +1,37 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+var createTaskHandler = function () {
+  event.preventDefault();
 
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  console.dir(taskNameInput);
 
-var createTaskHandler = function (){
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  console.log(taskTypeInput);
 
-    event.preventDefault();
-    
-    var listItemEl = document.createElement ("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task. ";
-    tasksToDoEl.appendChild(listItemEl);
-}
+  //creating our list items
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+
+  //create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+
+  //give it a class name
+  taskInfoEl.classname = "task-info";
+
+  //add html content to div
+  taskInfoEl.innerHTML =
+    "<h3 class = 'task-name'>" +
+    taskNameInput +
+    "</h3><span class='task-type'>" +
+    taskTypeInput +
+    "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
+
+  //add entire list item to list
+  tasksToDoEl.appendChild(listItemEl);
+};
 
 formEl.addEventListener("submit", createTaskHandler);
-
